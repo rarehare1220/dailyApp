14acd6bdd54019787267c9bfd760a6296b95aff7
double <double@dyn-118-139-55-21.its.monash.edu.au>
2015-05-13 15:55:03 +1000

    Initial Commit

---
+++ dailyTour/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  dailyTour
+//
+//  Created by double on 13/05/2015.
+//  Copyright (c) 2015 double. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
